8944fe6d48b880123f5a5c612ebb2ff344c6fc84
byy <1002842451@qq.com>
2019-08-01 16:25:01 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    APP
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
