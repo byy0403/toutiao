af555aad4fc23cbe0fd9c29b1fe8fd947e585799
byy <1002842451@qq.com>
2019-08-10 15:30:41 +0800

    配置富文本

---
+++ src/components/my-image.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="image-container">
+    <!-- 图片按钮 -->
+    <!--  <div class="img-btn"></div> -->
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
